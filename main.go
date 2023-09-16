7946eae8ae8b02d2426a487a00ef745cab01173f
Felippe Gaede <felippegaede@Felippes-MacBook-Pro.local>
2023-09-16 11:56:36 -0300

    adding load config from file and environment variables with Viper

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/felippegaede/simplebank/api"
 	db "github.com/felippegaede/simplebank/db/sqlc"
+	"github.com/felippegaede/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to to database", err)
 	}
@@ -24,10 +24,9 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
-
 }
