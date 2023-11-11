5a180a0c1c2d64c82b4d75c9131926923a92d513
Felippe Gaede <felippegaede@Felippes-MacBook-Pro.local>
2023-11-11 17:23:31 -0300

    adding refresh token

--- api/server.go
+++ api/server.go
@@ -45,6 +45,7 @@ func (server *Server) setupRouter() {
 
 	router.POST("/users", server.createUser)
 	router.POST("/login", server.loginUser)
+	router.POST("/renew_access", server.renewAccessToken)
 
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
 
